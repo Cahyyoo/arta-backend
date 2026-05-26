adb827ca1bd03ef0a6d411ba250f5159186359f5
cahyyoo <dwicahyomn@gmail.com>
2026-05-26 23:41:47 +0800

    update error env

--- src/config/supabaseAdmin.js
+++ src/config/supabaseAdmin.js
@@ -1,7 +1,6 @@
 const { createClient } = require('@supabase/supabase-js');
 require('dotenv').config();
 
-// Gunakan SERVICE_ROLE_KEY, bukan ANON_KEY
 const supabaseAdmin = createClient(
     process.env.SUPABASE_URL,
     process.env.SUPABASE_SERVICE_ROLE_KEY,
