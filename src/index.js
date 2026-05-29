e8b285a7ca13cfd654eefb1dd701fada0492538e
cahyyoo <dwicahyomn@gmail.com>
2026-05-29 20:05:28 +0800

    fix typo

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const cors = require("cors");
 const path = require("path");
 const apiRoutes = require("./routes/apiRoutes");
-const apiRoutes = require("./routes/authRoutes");
+const authRoutes = require("./routes/authRoutes");
 
 const app = express();
 
